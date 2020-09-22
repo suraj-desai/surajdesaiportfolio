b0b1e4f501e7e99d0ce0c185e924d38589be7477
Suraj <desaisuraj678@gmail.com>
2020-09-22 12:12:21 +0530

    Updates

---
+++ precache-manifest.aeafa080dd9e7548545ca44f24358958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ddf4d9e0cc715e69d793e9d84e785f",
+    "url": "/surajdesaiportfolio/index.html"
+  },
+  {
+    "revision": "762e39991775e030c7fd",
+    "url": "/surajdesaiportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "7adabef20f2117e967d1",
+    "url": "/surajdesaiportfolio/static/css/main.35de10b7.chunk.css"
+  },
+  {
+    "revision": "762e39991775e030c7fd",
+    "url": "/surajdesaiportfolio/static/js/2.c63e5153.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/surajdesaiportfolio/static/js/2.c63e5153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7adabef20f2117e967d1",
+    "url": "/surajdesaiportfolio/static/js/main.1fbd9322.chunk.js"
+  },
+  {
+    "revision": "9a79ca8fe2065298c2fa",
+    "url": "/surajdesaiportfolio/static/js/runtime-main.990dd4f0.js"
+  }
+]);
